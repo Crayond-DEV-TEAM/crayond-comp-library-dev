c6826629cd554652938caad0f096d0d7d1a13963
Hari Krishnan <harikrishnan@crayond.co>
2023-01-18 17:10:16 +0530

    feat: :triangular_flag_on_post: Updae the button component with style

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,7 +1,30 @@
-export function Button() {
+import React from 'react';
+import './button.css';
+
+interface ButtonProps {
+  primary?: boolean;
+  backgroundColor?: string;
+  size?: 'small' | 'medium' | 'large';
+  label: string;
+  onClick?: () => void;
+}
+
+export const Button = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary ? 'btn--primary' : 'btn--secondary';
   return (
-    <button>
-      A button
+    <button
+      type="button"
+      className={['btn', `btn--${size}`, mode].join(' ')}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
     </button>
   );
-}
+};
