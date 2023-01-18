bcc03aeaff2bb8a70cf2dc5fe599bc9290c15292
Hari Krishnan <harikrishnan@crayond.co>
2023-01-18 16:26:29 +0530

    chore: :bulb: Add comment for vite-plugin-dts plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 import { dependencies } from './package.json';
 import path, { resolve } from 'path';
 import dts from 'vite-plugin-dts';
+// A vite plugin that generates declaration files (*.d.ts) from .ts(x) when using vite in library mode.
+
+// .d.ts files are used to provide type information for JavaScript libraries
+// and modules, so that the TypeScript compiler can check the types of
+// variables and function calls in your code, even if the implementation is written in JavaScript.
 
 // https://vitejs.dev/config/
 export default defineConfig({
