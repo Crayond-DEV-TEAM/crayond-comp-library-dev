368045b3bfd0813f553486836fa700c1bedf35d4
Thilagaraj v <thilagarajvadivel99@gmail.com>
2023-04-06 11:28:27 +0530

    inputField Components is done

--- src/components/inputField/inputField.stories.tsx
+++ src/components/inputField/inputField.stories.tsx
@@ -1,9 +1,46 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { InputField } from './index'
+import SearchIcon from 'src/assets/searchIcon';
+import DeleteIcon from 'src/assets/deleteIcon';
 
 export default {
   title: 'components/InputField',
   component: InputField,
+  argTypes: {
+    value: {
+      description: 'The input value',
+    },
+    fullWidth: {
+      description: 'To set full width input field',
+    },
+    isReadOnly: {
+      description: 'To make it as readonly',
+    },
+    helperText: {
+      description: 'Text below input field',
+    },
+    isError: {
+      description: 'to show error message',
+    },
+    isMulti: {
+      description: 'For multi line',
+    },
+    rowMax: {
+      description: 'maximum rows',
+    },
+    rowMin: {
+      description: 'minimum rows',
+    },
+    placeholder: {
+      description: 'To set placeHolder',
+    },
+    type: {
+      description: 'input type',
+    },
+    size: {
+      description: 'size of the input field',
+    },
+  },
 
 } as ComponentMeta<typeof InputField>;
 
@@ -13,8 +50,7 @@ const Template: ComponentStory<typeof InputField> = (args) => (
 
 export const Primary = Template.bind({});
 Primary.args = {
-  //   name: 'Lable',
-  //   onChange: () => {},
-  //   value: true,
+  startAdornments: <SearchIcon />,
+  endAdornments: <DeleteIcon />
 };
 
--- src/components/inputField/inputField.tsx
+++ src/components/inputField/inputField.tsx
@@ -1,32 +1,77 @@
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
-import { InputFieldProps } from './props'
-import { inputField_Style } from './style'
+import TextField from '@mui/material/TextField';
+import { InputFieldProps } from './props';
 
-export default function InputField(
-    props: InputFieldProps) {
+
+export default function InputField(props: InputFieldProps) {
+
+    const {
+        value = '',
+        fullWidth = true,
+        isReadOnly = false,
+        helperText = '',
+        isError = false,
+        isMulti = false,
+        rowMax = 5,
+        rowMin = 5,
+        placeholder = '',
+        size = 'small',
+        onChange = () => false,
+        endAdornments = '',
+        startAdornments,
+        type = '',
+        errorMessage = '',
+        variant = 'outlined',
+        textFieldStyle = {},
+        className = '',
+        defaultValue,
+        ...rest
+    } = props;
 
     return (
-        <TextField />
+        <TextField
+            type={type}
+            size={size}
+            variant={variant}
+            value={value}
+            placeholder={placeholder}
+            fullWidth={fullWidth}
+            disabled={isReadOnly}
+            defaultValue={defaultValue}
+            multiline={isMulti}
+            maxRows={rowMax}
+            minRows={rowMin}
+            onChange={onChange}
+            helperText={helperText}
+            error={!!isError}
+            className={`${className}`}
+            InputProps={{
+                startAdornment: (
+                    <InputAdornment position="start">{startAdornments}</InputAdornment>
+                ),
+                endAdornment: (
+                    <InputAdornment position="end">{endAdornments}</InputAdornment>
+                ),
+            }}
+        />
     );
 }
 
 InputField.defaultProps = {
-    type: '',
-    size: '',
-    rowMax: '',
-    rowMin: '',
-    isError: '',
-    variant: '',
-    helperText: '',
-    placeholder: '',
+    variant: 'outlined',
+    value: '',
     onChange: () => { },
-    value: false,
     disabled: false,
+    type: 'text',
+    size: 'small',
+    placeholder: '',
+    endIcon: '',
     fullwidth: false,
     isMulti: false,
-    endIcon: null,
-    startAdornment: '',
-    endAdornments: ''
+    rowMax: '',
+    rowMin: '',
+    helperText: '',
+    isError: '',
+    classes: {},
+    sx: {},
 };
