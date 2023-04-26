0b44e2cbf9641d2f489933b734c7c30063903651
Akila kalaiyarasan <akila@crayond.co>
2023-04-26 18:29:52 +0530

    initial commit

---
+++ src/components/signUpPage/signUp.tsx
@@ -0,0 +1,287 @@
+import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import {
+  Box,
+  Divider,
+  Grid,
+  Hidden,
+  InputAdornment,
+  Typography,
+} from '@mui/material';
+import React from 'react';
+
+import Card from '../card/card';
+import { Image } from '../image';
+import { InputField } from '../inputField';
+import { MobileInput } from '../mobileInput';
+import { SocialMediaButton } from '../socialMediaButton';
+import { signUp_style } from './style';
+
+type SignupOption =
+  | 'emailWithPasswordSignup'
+  | 'socialMediaSignup'
+  | 'mobileNumberSignup';
+
+export interface SignupScreen {
+  onSubmit: (details: object) => void;
+  option: SignupOption;
+  cardData: any;
+  backgroundImg: any;
+  rootStyle: object;
+  cardWraperStyle: object;
+}
+
+const SignupScreen: React.FC<SignupScreen> = ({
+  option,
+  onSubmit,
+  cardData,
+  backgroundImg,
+  rootStyle,
+  cardWraperStyle,
+}) => {
+  const type = option;
+  const socialMedia = cardData.socialMediaDetails;
+  const [email, setEmail] = React.useState('');
+  const [mobileNumber, setMobileNumber] = React.useState('');
+  const [countryCode, setCountryCode] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [errorMail, setErrorMail] = React.useState(false);
+  const [errorPassword, setErrorPassword] = React.useState(false);
+  const [errorMsg, setErrorMsg] = React.useState('');
+  const [errorPasswordMsg, setErrorPasswordMsg] = React.useState('');
+  const [passwordvisible, setPasswordVisible] = React.useState(false);
+
+  const getMailValue = (e: any) => {
+    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const value = e.target.value;
+    if (regex.test(value)) {
+      setErrorMail(false);
+      setEmail(value);
+      setErrorMsg('');
+    } else {
+      setErrorMsg('Please enter your registered email');
+      setErrorMail(true);
+    }
+  };
+
+  const getPasswordValue = (e: any) => {
+    const value = e.target.value;
+    const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/;
+    if (regex.test(value)) {
+      setErrorPassword(false);
+      setPassword(value);
+      setErrorPasswordMsg('');
+    } else {
+      setErrorPasswordMsg('Please enter password');
+      setErrorPassword(true);
+    }
+  };
+  const getThrowErrorMsg = () => {
+    switch (type) {
+      case 'emailWithPasswordSignup':
+        if (email === '') {
+          setErrorMsg('Please enter your registered email');
+          setErrorMail(true);
+        }
+        if (password === '') {
+          setErrorPasswordMsg('Please enter password');
+          setErrorPassword(true);
+        }
+        if (email !== '' && password !== '') {
+          const userDetail = {
+            email: email,
+            password: password,
+            type: type,
+          };
+          onSubmit(userDetail);
+        }
+      case 'mobileNumberSignup':
+        if (mobileNumber !== '') {
+          const userDetail = {
+            mobile_number: mobileNumber,
+            countryCode: countryCode,
+            type: type,
+          };
+          onSubmit(userDetail);
+        }
+    }
+  };
+
+  const getMobileNumber = (value: any) => {
+    setMobileNumber(value.mobile);
+    setCountryCode(value.mobile_code);
+  };
+  const passWordVisible = () => {
+    setPasswordVisible(passwordvisible ? false : true);
+  };
+  const getoptionretrieve = (option: string) => {
+    switch (option) {
+      case 'mobileNumberSignup':
+        return (
+          <>
+            <Typography sx={signUp_style.labelSx}>Mobile Number</Typography>
+            <MobileInput
+              handleChange={getMobileNumber}
+              rootWapperstyle={signUp_style.textFieldSx}
+            />
+          </>
+        );
+      case 'socialMediaSignup':
+        return (
+          <>
+            {socialMedia?.map((item: any) => {
+              return (
+                <SocialMediaButton
+                  rootStyle={signUp_style.socialButtonSx}
+                  startIcon={item?.icon}
+                  buttonText={item?.label}
+                  onClick={item?.onSocialmediaLogin}
+                  socialmediaTextStyle={item?.style}
+                />
+              );
+            })}
+            <Divider sx={signUp_style.dividerSx}>
+              <Typography sx={signUp_style.fontSx}>or</Typography>
+            </Divider>
+            <Typography sx={signUp_style.labelSx}>Work Email</Typography>
+            <InputField
+              fullWidth
+              size="small"
+              error={errorMail}
+              helperText={errorMsg}
+              onChange={getMailValue}
+              textFieldStyle={signUp_style.textFieldSx}
+            />
+            <Typography sx={signUp_style.forgotSx}>
+              <span
+                onClick={cardData?.onForgotClick}
+                style={{ cursor: 'pointer' }}
+              >
+                Forgot Password?
+              </span>
+            </Typography>{' '}
+          </>
+        );
+      default:
+        return (
+          <>
+            <Box sx={{ display: 'flex',gap:1 }}>
+              <Box>
+                <Typography sx={signUp_style.labelSx}>First Name</Typography>
+                <InputField
+                  fullWidth
+                  size="small"
+                  textFieldStyle={signUp_style.textFieldSx}
+                />
+              </Box>
+              <Box>
+                <Typography sx={signUp_style.labelSx}>Last Name</Typography>
+                <InputField
+                  fullWidth
+                  size="small"
+                  textFieldStyle={signUp_style.textFieldSx}
+                />
+              </Box>
+            </Box>
+            <Typography sx={signUp_style.labelSx}>Email</Typography>
+            <InputField
+              fullWidth
+              size="small"
+              error={errorMail}
+              helperText={errorMsg}
+              onChange={getMailValue}
+              textFieldStyle={signUp_style.textFieldSx}
+            />
+            <Typography sx={signUp_style.labelSx}>Password</Typography>
+            <InputField
+              fullWidth
+              size="small"
+              type={passwordvisible ? 'text' : 'password'}
+              error={errorPassword}
+              helperText={errorPasswordMsg}
+              onChange={getPasswordValue}
+              textFieldStyle={signUp_style.textFieldSx}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment
+                    style={{ cursor: 'pointer' }}
+                    onClick={passWordVisible}
+                    position="end"
+                  >
+                    {passwordvisible ? (
+                      <VisibilityOutlinedIcon />
+                    ) : (
+                      <VisibilityOffOutlinedIcon />
+                    )}
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <Typography sx={signUp_style.forgotSx}>
+              <span
+                onClick={cardData?.onForgotClick}
+                style={{ cursor: 'pointer' }}
+              >
+                Forgot Password?
+              </span>
+            </Typography>
+          </>
+        );
+    }
+  };
+  return (
+    <Grid container sx={{ ...signUp_style.parentSx, ...rootStyle }}>
+      {backgroundImg && (
+        <Hidden smDown>
+          <Grid
+            item
+            xs={12}
+            sm={4}
+            md={3}
+            sx={backgroundImg?.backgroundWrapStyle}
+          >
+            <Image
+              src={backgroundImg?.imgSrc}
+              width={'100%'}
+              height={'100%'}
+              imageStyle={backgroundImg?.bgImageStyle}
+            />
+          </Grid>
+        </Hidden>
+      )}
+      <Grid
+        item
+        xs={12}
+        sm={backgroundImg ? 8 : 12}
+        md={backgroundImg ? 9 : 12}
+        sx={{ ...signUp_style.loginSx, ...cardWraperStyle }}
+      >
+        <Box sx={signUp_style.cardParentSx}>
+          <Card
+            title={cardData?.title}
+            logo={cardData?.logo}
+            description={cardData?.description}
+            children={
+              <Box sx={signUp_style.childernSx}>
+                {getoptionretrieve(option)}
+              </Box>
+            }
+            bottomText={cardData?.bottomText}
+            button={cardData?.button}
+            btnClick={getThrowErrorMsg}
+            buttonText={cardData?.buttonText}
+            cardStyle={{ ...signUp_style.cardSx, ...cardData?.cardStyle }}
+            btnStyle={{ ...signUp_style.loginBtnSx, ...cardData?.btnStyle }}
+            actionText={cardData?.loginActionText}
+            actionstyle={{ ...signUp_style.actionSx, ...cardData?.actionstyle }}
+            bottomTextStyle={signUp_style.bottomTextSx}
+            onActionClick={cardData?.onSignUpClick}
+            imgStyle={signUp_style.logoSx}
+          />
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default SignupScreen;
