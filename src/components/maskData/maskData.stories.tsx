0cf14335827748ab2365f754a4e879f6ea60aec4
Hariram <hariram@CLLAP-CHN-1744.local>
2023-05-03 17:42:34 +0530

    testing

--- src/components/maskData/maskData.stories.tsx
+++ src/components/maskData/maskData.stories.tsx
@@ -1,19 +1,19 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { IconWithText } from './index';
-import FunnelIcon from '../../assets/funnelIcon';
+// import { ComponentStory, ComponentMeta } from '@storybook/react';
+// import { IconWithText } from './index';
+// import FunnelIcon from '../../assets/funnelIcon';
+export default {}
+// export default {
+//   title: 'CommonTable/IconWithText',
+//   component: IconWithText,
+// } as ComponentMeta<typeof IconWithText>;
 
-export default {
-  title: 'CommonTable/IconWithText',
-  component: IconWithText,
-} as ComponentMeta<typeof IconWithText>;
+// const Template: ComponentStory<typeof IconWithText> = (args) => (
+//   <IconWithText {...args} />
+// );
 
-const Template: ComponentStory<typeof IconWithText> = (args) => (
-  <IconWithText {...args} />
-);
-
-export const Primary = Template.bind({});
-Primary.args = {
-  label: 'Sent',
-  icon: <FunnelIcon />,
-};
+// export const Primary = Template.bind({});
+// Primary.args = {
+//   label: 'Sent',
+//   icon: <FunnelIcon />,
+// };
 
