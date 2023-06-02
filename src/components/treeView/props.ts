3a986dd4ae69a7e1a5ac80dcd410bc579abef64b
Hari ram <admin@admins-MacBook-Pro.local>
2023-06-02 12:17:48 +0530

    porps changes done

--- src/components/treeView/props.ts
+++ src/components/treeView/props.ts
@@ -1,40 +1,39 @@
 import { SxProps, Theme } from '@mui/material';
 import { ReactElement } from 'react';
 
+interface PermissionLogic {}
+interface DataRowLogic {
+    map(arg0: (e: any, parentIndex: number) => JSX.Element): import("react").ReactNode;
+    [index: number]: { title: string; permission:PermissionLogic };
+}
 export interface TreeViewProps {
     sx?: SxProps<Theme>;
-    rootStyle: object;
-    formControlPropsSx: object;
+    rootStyle?: object;
+    formControlPropsSx?: object;
     onSubmit: () => void;
-    heading: ReactElement | string,
-    permissionHeadingSx: object;
-    checkboxTitleSx: object;
-    submitBtnText: ReactElement | string;
-    submitBtnOptions: {
-        variant: string,
+    heading?: ReactElement | string,
+    permissionHeadingSx?: object;
+    checkboxTitleSx?: object;
+    submitBtnText?: ReactElement | string;
+    submitBtnOptions?: {
+        variant: "contained" | "text" | "outlined" | undefined,
         bgColor: string,
         textColor: string
     };
     handleChange: (e: any, val: string, parentIndex: number) => void;
-    checkboxIcon: ReactElement;
-    uncheckedIcon: ReactElement;
-    checkboxWidth: number;
-    checkboxBorderRadius: string | number;
-    checkboxHeight: number;
-    checkboxBgColor: string;
-    dataRow: [
-        {
-            title: any,
-            permission: {
-            }
-        },
-    ];
-    leftSec: {
+    checkboxIcon?: ReactElement;
+    uncheckedIcon?: ReactElement;
+    checkboxWidth?: number;
+    checkboxBorderRadius?: string | number;
+    checkboxHeight?: number;
+    checkboxBgColor?: string;
+    dataRow: DataRowLogic;
+    leftSec?: {
         breakpoints: object;
     };
-    rightSec: {
+    rightSec?: {
         breakpoints: object;
     }
-    dividerPropsSx: object
+    dividerPropsSx?: object
 
 }
