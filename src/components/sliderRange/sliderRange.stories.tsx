0421e8772db4debd22904348a43902ddbc834bc6
Henry <henrydysonaricjasan1310@gmail.com>
2023-06-13 10:44:44 +0530

    add story book discription

--- src/components/sliderRange/sliderRange.stories.tsx
+++ src/components/sliderRange/sliderRange.stories.tsx
@@ -10,26 +10,112 @@ const Template: ComponentStory<typeof SliderRange> = (args) => (
 );
 
 export const Primary = Template.bind({});
+Primary.argTypes = {
+  value: {
+    description: 'number',
+    defaultValue: 33,
+  },
+  step: {
+    description: 'number',
+    defaultValue: 10,
+  },
+  labelStyle: {
+    description: 'object',
+    defaultValue: {},
+  },
+  sliderHeight: {
+    description: 'number',
+    defaultValue: 6,
+  },
+  size: {
+    description: '"small"| "medium"|"large"',
+    defaultValue: 'small',
+  },
+  disabled: {
+    description: 'boolean',
+    defaultValue: false,
+  },
+  customMarks: {
+    description: 'boolean',
+    defaultValue: false,
+  },
+  thumbColor: {
+    description: 'string',
+    defaultValue: '#665CD7',
+  },
+  minLabel: {
+    description: 'string',
+    defaultValue: 'min',
+  },
+  maxLabel: {
+    description: 'string',
+    defaultValue: 'max',
+  },
+  valueLabelDisplay: {
+    description: '"off"| "on"',
+    defaultValue: 'off',
+  },
+  sliderColor: {
+    description: 'string',
+    defaultValue: '#665CD7',
+  },
+  markLabelColor: {
+    description: 'string',
+    defaultValue: "'#707070",
+  },
+  minMaxValues: {
+    description: 'boolean',
+    defaultValue: false,
+  },
+  markLabelSize: {
+    description: 'number',
+    defaultValue: 12,
+  },
+  handleChange: {
+    description: '()=>{}',
+    defaultValue: '',
+  },
+  sliderStyle: {
+    description: 'object',
+    defaultValue: {},
+  },
+  minMaxLabel: {
+    description: 'string',
+    defaultValue: '',
+  },
+  containerStyle: {
+    description: 'object',
+    defaultValue: {},
+  },
+  sliderThumbStyle: {
+    description: 'object',
+    defaultValue: {},
+  },
+  marks: {
+    description: 'Array of object ',
+    defaultValue: [{ label: '0*C', value: 0 }],
+  },
+};
 Primary.args = {
   // value: [20,30],
-  handleChange:()=>{},
-  markLabelColor:"#707070",
-  markLabelSize:12,
-  minLabel:"Min",
-  maxLabel:"Max",
-  sliderHeight:8,
-  sliderColor:"#665CD7",
-  thumbColor:"#665CD7",
-  disabled:false,
-  valueLabelDisplay:"off",
-  step:10,
-  customMarks:false,
-  minMaxValues:false,
+  handleChange: () => {},
+  markLabelColor: '#707070',
+  markLabelSize: 12,
+  minLabel: 'Min',
+  maxLabel: 'Max',
+  sliderHeight: 8,
+  sliderColor: '#665CD7',
+  thumbColor: '#665CD7',
+  disabled: false,
+  valueLabelDisplay: 'off',
+  step: 10,
+  customMarks: false,
+  minMaxValues: false,
   sliderStyle: {},
   minMaxLabel: {},
   labelStyle: {},
   containerStyle: {},
-  sliderThumbStyle:{},
+  sliderThumbStyle: {},
   marks: [
     {
       value: 0,
