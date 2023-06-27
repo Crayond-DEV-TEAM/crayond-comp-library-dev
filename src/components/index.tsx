f96c32129ae28503bd8f30e1a7618593bb7a1ca0
Henry <henrydysonaricjasan1310@gmail.com>
2023-06-27 14:19:03 +0530

    update index file

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,5 @@ export { BasicButtons } from './button'
 export { CommonTable } from './table';
 export { Switch } from './switch';
 export { Progress } from './progress';
+export {SingleInputDateRangePicker}from "./dateRangePicker";
+export {SingleDatePicker} from './datePicker';
