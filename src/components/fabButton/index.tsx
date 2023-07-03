06d3a387701e7f21c4208ca7a7a88416f9ce3475
Akila kalaiyarasan <akila@crayond.co>
2023-07-03 13:06:31 +0530

    fab button initial commit

---
+++ src/components/fabButton/index.tsx
@@ -0,0 +1,11 @@
+import { Card as MuiCard, Typography, Box, CardActions } from '@mui/material';
+import React from 'react';
+import { FabProps } from './props';
+
+const Fab: React.FC<FabProps> = ({}) => {
+  return <Box></Box>;
+};
+
+export default Fab;
+
+Fab.defaultProps = {};
