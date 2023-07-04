1540b4d2dee605b701003ed90b10b2a78690257a
Arunachalam <arunachalam@crayond.co>
2023-07-04 11:21:55 +0530

    initial commit for pricing

---
+++ src/components/pricingCard/index.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import {Box} from '@mui/material';
+import { pricingCard_Style } from './style';
+  export function CustomCalender(props: any) {
+    const {} = props;
+
+    return (
+      <Box sx={{...pricingCard_Style.rootSx }}>
+       
+      </Box>
+    );
+  }
+  
+  CustomCalender.defaultProps = {
+  
+  };
+  
---
+++ src/components/pricingCard/style.ts
@@ -0,0 +1,6 @@
+export const pricingCard_Style = {
+
+    rootSx:{
+        
+    }
+};
