010688cf8a47229648bec59ffa4929d2382d8c9c
Black paiyan <henry@crayond.co>
2023-07-20 14:19:54 +0530

    completed compare feature

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,7 @@
+
 export { BasicButtons } from './button'
 export { CommonTable } from './table';
 export { Switch } from './switch';
 export { Progress } from './progress';
 export { SelectBoxComponent } from './selectBox'
+export { CompareFeature } from './compareFeature';
