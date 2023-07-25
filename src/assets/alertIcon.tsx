29702d92e185b767c8fa3361d926ce8486ae0620
Black paiyan <henry@crayond.co>
2023-07-25 14:28:08 +0530

    clear conflict

--- src/assets/alertIcon.tsx
+++ src/assets/alertIcon.tsx
@@ -1,5 +1,5 @@
 const AlertIcon = (props: any) => (
-<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
+<svg xmlns="http://www.w3.org/2000/svg" width="68" height="68" viewBox="0 0 48 48">
   <g id="Group_106303" data-name="Group 106303" transform="translate(-667 -316)">
     <circle id="Ellipse_129836" data-name="Ellipse 129836" cx="24" cy="24" r="24" transform="translate(667 316)" fill="#fccacd"/>
     <path id="icons8-error" d="M16.02,5.053a2.675,2.675,0,0,0-2.372,1.367l-9.3,16.8a2.741,2.741,0,0,0,2.372,4.027h18.6a2.741,2.741,0,0,0,2.372-4.027l-9.3-16.8A2.675,2.675,0,0,0,16.02,5.053Zm0,1.74a.911.911,0,0,1,.8.5l9.3,16.8a.885.885,0,0,1-.8,1.353H6.723a.885.885,0,0,1-.8-1.353l9.3-16.8A.911.911,0,0,1,16.02,6.792Zm-.014,4.822a.9.9,0,0,0-.887.914v6.006a.9.9,0,1,0,1.8,0V12.529a.9.9,0,0,0-.915-.914Zm.014,9.623a1.2,1.2,0,1,0,1.2,1.2A1.2,1.2,0,0,0,16.02,21.237Z" transform="translate(674.979 323.852)" fill="#f44f5a"/>
