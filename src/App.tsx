70552f56b0074b556d60c757d80fbf37cdd8b5e8
hariharanCrayond <hariharan@crayond.co>
2023-07-25 11:12:29 +0530

    feat: app.tsx changed

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import '@fontsource/poppins/700.css';
 
 function App() {
   return (
-    <div className="App" style={{ width: '100vw', height: '100vh' }}></div>
+    <div className="App" style={{ width: '100vw', height: '100vh' }}>Check  <code>App.tsx</code></div>
   );
 }
 
