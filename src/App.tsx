f5540c3e16dfb87577c0aded92b4899dc23647ba
Black paiyan <henry@crayond.co>
2023-07-26 14:36:14 +0530

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,6 @@ import '@fontsource/poppins/600.css';
 import '@fontsource/poppins/700.css';
 import { DragAndDrop } from './components';
 function App() {
-  const initialColumns = [
-    {
-      id: 1,
-      status:"Progress",
-      list: ['item 1', 'item 2', 'item 3', 'item 4'],
-    },
-    {
-      id: 2,
-      status:"Pending",
-      list: [],
-    },
-    {
-      id: 3,
-      status:"completed",
-      list: [],
-    },
-  ];
 
   return (
     <div className="App" style={{ width: '100vw', height: '100vh' }}>
