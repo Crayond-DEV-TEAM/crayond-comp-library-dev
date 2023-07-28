c644053440cdb588a2b4d24c9d6d868bf5a386f9
Black paiyan <henry@crayond.co>
2023-07-28 11:14:20 +0530

    completed drag hover border

--- src/components/DragAndDrop/props.tsx
+++ src/components/DragAndDrop/props.tsx
@@ -1,3 +1,51 @@
-export interface DragAndDropProps {
-    
+export interface DragAndDropProps {}
+export interface DragStart {
+  draggableId: number;
+  mode: string;
+  type: string;
+  source: {
+    droppableId: string;
+    index: number;
+  };
+}
+export interface DragUpdate {
+  combine: null;
+  destination: {
+    droppableId: string;
+    index: number;
+  };
+  draggableId: number;
+  mode: string;
+  source: {
+    droppableId: string;
+    index: number;
+  };
+  type: string;
+}
+
+export interface DragEndColumn {
+    requested: {
+        name: string;
+        items: [],
+        bottomButton: {
+          icon:React.ReactNode,
+          buttonLabel:string,
+        },
+      },
+      inProgress: {
+        name: string;
+        items: [],
+        bottomButton: {
+          icon:React.ReactNode,
+          buttonLabel:string,
+        },
+      },
+      done: {
+        name: string;
+        items: [],
+        bottomButton: {
+          icon:React.ReactNode,
+          buttonLabel:string,
+        },
+      },    
 }
